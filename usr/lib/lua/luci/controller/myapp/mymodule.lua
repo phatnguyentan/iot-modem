39358c559eac8be2e33d7b3fe26924f243c64b4e
David Nguyen <david@wthapps.com>
2019-05-28 15:25:39 +0700

    update

--- usr/lib/lua/luci/controller/myapp/mymodule.lua
+++ usr/lib/lua/luci/controller/myapp/mymodule.lua
@@ -3,42 +3,9 @@ module("luci.controller.myapp.mymodule", package.seeall)
 function index()
     entry({"my", "new", "template"}, template("myapp-mymodule/helloworld"), "Hello world", 20).dependent=false
     entry({"my", "new", "background"}, template("myapp-mymodule/helloworld"), "Hello world", 20).dependent=false
-    entry({"my", "new", "upload"}, call("uploader")).dependent=false
-end
-
-function upload_background()
-	local fs = require "nixio.fs"
-	local http = require "luci.http"
-  local archive_tmp = "/etc/nodogsplash/htdocs/images/bg3.jpg"
-  local fp
-  
-	http.setfilehandler(
-		function(meta, chunk, eof)
-			if not fp and meta and meta.name == "archive" then
-				fp = io.open(archive_tmp, "wb")
-			end
-			if fp and chunk then
-				fp:write(chunk)
-			end
-			if fp and eof then
-				fp:close()
-			end
-		end
-  )
-
-  -- local f = http.formvalue("archive")[0]
-  -- upload = {}
-  -- if f then
-  --     local name = "b3.jpg"
-  --     local bytes = f
-  --     local dest = io.open("/etc/nodogsplash/htdocs/images" .. "/" .. name, "wb")
-  --     if dest then
-  --         dest:write(bytes)
-  --         dest:close()
-  --     upload[1] = name
-  --     end
-  -- end
-  http.redirect(luci.dispatcher.build_url('my/new/template'))
+		entry({"my", "new", "upload"}, call("uploader")).dependent=false
+		entry({"my", "new", "fanpage"}, template("myapp-mymodule/fanpage")).dependent=false
+		entry({"my", "update", "page"}, call("copy_config")).dependent=false
 end
 
 function uploader()
@@ -46,7 +13,7 @@ function uploader()
    --add your defaults (file_name is optional btw)
    file_loc = "/etc/nodogsplash/htdocs/images/"
    input_field = "archive"
-   file_name = "b3.jpg"
+   file_name = "default"
    local http = require "luci.http"
    --actually call the file handler
    --get the values from the forms on the page
@@ -91,4 +58,21 @@ function setFileHandler(location, input_name, file_name)
 			fp:close()
 		 end
 		 end)
+end
+
+function copy_config()
+	local http = require "luci.http"
+	local values = luci.http.formvalue()
+	local link = values["link"]
+	local infile = io.open("/usr/lib/lua/luci/controller/myapp/nodogsplash.conf", "r")
+	local instr = infile:read("*a")
+	infile:close()
+
+	instr = string.gsub(instr, "redirect_url", link)
+
+	local outfile = io.open("/etc/nodogsplash/nodogsplash.conf", "w")
+	outfile:write(instr)
+	outfile:close()
+	os.execute("/etc/init.d/nodogsplash restart")
+	http.redirect(luci.dispatcher.build_url('my/new/fanpage'))
 end
